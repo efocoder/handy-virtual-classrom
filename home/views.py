1eda9672279fcebea3f66da888de6ffea832a6b9
clemsedem <clemsedem@gmail.com>
2020-04-10 19:51:09 +0000

    'home'

--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from datetime import date
+
 
 def index(request):
-    return render(request, 'home/index.html')
+    current_year = date.today().year
+    context= {
+        'copyright': current_year
+    }
+    return render(request, 'home/index.html', context)

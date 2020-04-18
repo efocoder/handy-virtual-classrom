69f497471ee608e8f1ed0b9c760153d048fa8e19
clemsedem <clemsedem@gmail.com>
2020-04-18 01:13:06 +0000

    'static-root'

--- handy_virtual_class/settings.py
+++ handy_virtual_class/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'materializecssform',
     'home',
     'accounts',
     'landing',
@@ -121,13 +120,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # SENDGRID_API_KEY =  #os.getenv('SENDGRID_API_KEY')
 

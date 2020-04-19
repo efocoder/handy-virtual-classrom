e61113f84d3e06aabcbc179ba99dae5e806523c3
clemsedem <clemsedem@gmail.com>
2020-04-19 19:38:32 +0000

    django-key

--- handy_virtual_class/settings.py
+++ handy_virtual_class/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w*%x)#ox_-^l2x_yd_v3%xvn5httm=573hlh#)6jruc-q3_0o2'
+SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ADMIN_ENABLED = False
 
 ALLOWED_HOSTS = ['handy-vc.herokuapp.com', '127.0.0.1', 'localhost']
